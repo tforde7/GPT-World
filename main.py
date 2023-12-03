369e6548afb05bf97192a96be42d923f7462214b
Tom Forde <tforde@atmospheremxt.com>
2023-12-03 15:11:39 +0000

    added create player button

--- main.py
+++ main.py
@@ -76,19 +76,21 @@ def main_menu():
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
-        MENU_RECT = MENU_TEXT.get_rect(center=(CENTRE_X, 100))
+        MENU_TEXT = get_font(100).render("GPT World", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(CENTRE_X, 65))
 
-        PLAY_BUTTON = Button(image=pygame.image.load("assets/img/play-button.png"), pos=(CENTRE_X, 250),
-                             text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/img/options-button.png"), pos=(CENTRE_X, 400),
-                                text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        QUIT_BUTTON = Button(image=pygame.image.load("assets/img/quit-button.png"), pos=(CENTRE_X, 550),
-                             text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        PLAY_BUTTON = Button(image=pygame.image.load("assets/img/button-rect.png"), pos=(CENTRE_X, 200),
+                             text_input="PLAY", font=get_font(40), base_color="#d7fcd4", hovering_color="White")
+        CREATE_PLAYER_BUTTON = Button(image=pygame.image.load("assets/img/button-rect.png"), pos=(CENTRE_X, 350),
+                                text_input="CREATE PLAYER", font=get_font(40), base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/img/button-rect.png"), pos=(CENTRE_X, 500),
+                                text_input="OPTIONS", font=get_font(40), base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/img/button-rect.png"), pos=(CENTRE_X, 650),
+                             text_input="QUIT", font=get_font(40), base_color="#d7fcd4", hovering_color="White")
 
         WINDOW.blit(MENU_TEXT, MENU_RECT)
 
-        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON, CREATE_PLAYER_BUTTON]:
             button.changeColor(MENU_MOUSE_POS)
             button.update(WINDOW)
 
@@ -101,9 +103,37 @@ def main_menu():
                     play()
                 if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
                     options()
+                if CREATE_PLAYER_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    create_player()
                 if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
                     pygame.quit()
                     sys.exit()
+
+        pygame.display.update()
+
+def create_player():
+    while True:
+        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+
+        WINDOW.fill("black")
+
+        HEADER = get_font(45).render("CREATE PLAYER", True, "white")
+        HEADER_RECT = HEADER.get_rect(center=(CENTRE_X, 50))
+        WINDOW.blit(HEADER, HEADER_RECT)
+
+        OPTIONS_BACK = Button(image=None, pos=(CENTRE_X, 560),
+                              text_input="BACK", font=get_font(45), base_color="white", hovering_color="Green")
+
+        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+        OPTIONS_BACK.update(WINDOW)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+                    main_menu()
 
         pygame.display.update()
 
